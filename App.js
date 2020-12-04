8f3754ad27e2f0bc2c5d82099af7563e233c24b5
Igor Lasota <igorlasota07@gmail.com>
2020-12-04 15:16:03 +0100

    Added First Version of Header

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text, SafeAreaView, StatusBar } from 'react-native';
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={{fontSize: 25, }}>Brain Benchmark</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -15,7 +17,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  header: {
+    padding: 5,
+    backgroundColor: '#3ac1e3',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    minHeight: '10%',
+    justifyContent: 'center',
+  }
 });
