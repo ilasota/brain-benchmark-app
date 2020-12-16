b9ee18ca7eefb603a73ac3d65dfb05dedbbf4a20
Igor Lasota <igorlasota07@gmail.com>
2020-12-16 18:19:38 +0100

    Fixed numberHandler

--- components/GameOne.js
+++ components/GameOne.js
@@ -4,28 +4,35 @@ import { StyleSheet, View, Text, SafeAreaView, StatusBar, TouchableOpacity, Text
 
 function GameOne () {
     const [ number, setNumber] = useState(Math.round(Math.random()*10));
+    const [ round, setRound ] = useState(1);
 
-    let roundNum = 1;
     let powerOfTen;
+    let dupa;
+    let randomNum;
 
     const numberHandler = () => {
-        roundNum += 1;
-        console.log(roundNum);
-        powerOfTen = Math.pow(10, roundNum);
-        console.log(powerOfTen);
-        let randomNum = Math.random();
-        console.log(randomNum);
-        let dupa = randomNum * powerOfTen;
-        console.log(dupa);
+        setRound(round + 1);
+        powerOfTen = Math.pow(10, round + 1);
+        randomNum = Math.random();
+        if (randomNum < 0.1) {
+            dupa = Math.floor( (powerOfTen / 10) + (randomNum * powerOfTen) );
+        } else {
+            dupa = Math.floor( randomNum * powerOfTen );
+        }
+        setNumber(dupa);
     }
 
 
     return (
       <SafeAreaView style={styles.container}>
           <View>
+              <Text>{round}</Text>
               <Text>{number}</Text>
               <TouchableOpacity onPress={numberHandler}>
-                  <Text>Change</Text>
+                  <Text style={{fontSize: 20}}>Change</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => setNumber()}>
+                  <Text style={{fontSize: 20}}>Change</Text>
               </TouchableOpacity>
           </View>
       </SafeAreaView>
