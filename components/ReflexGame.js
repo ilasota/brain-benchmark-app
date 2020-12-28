1a383527c2f714a6382a3020604c6a36afb1f89c
Igor Lasota <igorlasota07@gmail.com>
2020-12-28 14:42:59 +0100

    Added Start Screen to ReflexGame.js

--- components/ReflexGame.js
+++ components/ReflexGame.js
@@ -6,10 +6,37 @@ import { StyleSheet, View, Text, SafeAreaView, StatusBar, TouchableOpacity, Text
 function ReflexGame () {
 
     return (
-        <View>
-            <Text>Dupa</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <View>
+                <View style={styles.startGame}>
+                    <TouchableOpacity style={styles.startButton}>
+                        <Text style={ {fontSize: 30} }>Start</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#3ac1e3',
+        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+        minHeight: '10%',
+    },
+    startGame:{
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: "50%",
+    },
+    startButton: {
+        backgroundColor: "#62d653",
+        paddingVertical: 5,
+        paddingHorizontal: 10,
+        borderWidth: 1,
+        borderRadius: 15,
+    },
+})
 
 export default ReflexGame
