7500b4a8ca1adae71f34ab2a26ab5ba3cd64fd5b
Igor Lasota <igorlasota07@gmail.com>
2020-12-28 13:32:46 +0100

    Added ReflexGame.js Component

---
+++ components/ReflexGame.js
@@ -0,0 +1,15 @@
+import 'react-native-gesture-handler';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, SafeAreaView, StatusBar, TouchableOpacity, TextInput, Keyboard, Image } from 'react-native';
+
+
+function ReflexGame () {
+
+    return (
+        <View>
+            <Text>Dupa</Text>
+        </View>
+    )
+}
+
+export default ReflexGame
