7dcdfb8b680821d1ccb33bc26f4b687e0cbc7183
Igor Lasota <igorlasota07@gmail.com>
2021-02-08 16:00:50 +0100

    Added Start Screen

--- components/ChimpGame.js
+++ components/ChimpGame.js
@@ -5,13 +5,16 @@ import {StyleSheet, View, Text, SafeAreaView, StatusBar, FlatList, TouchableOpac
 
 
 function ChimpGame () {
+  const [ startVisible, setStartVisible ] = useState({display: "flex"});
+  const [ gameVisible, setGameVisible ] = useState({display: "none"});
   const [gameBoard, setGameBoard]  = useState([]);
+
   let squarePosition = Array(36);
   let position = [];
 
 
     const boardHandler = () => {
-        for(let i = 0; i < 10; i++) {
+        for(let i = 0; i < 4; i++) {
             let pos = Math.floor(Math.random() * 35);
             while (position.includes(pos)) {
                 pos = Math.floor(Math.random() * 35);
@@ -20,32 +23,70 @@ function ChimpGame () {
         }
 
         for(let i = 0; i < 36; i++){
-            squarePosition[i] = {value: " ", event: () =>{}, id: Math.random()};
+            squarePosition[i] = {
+                value: " ",
+                event: () =>{},
+                id: Math.random(),
+                test: {
+                    minWidth: 55,
+                    minHeight: 55,
+                    margin: 5,
+                }
+            };
         }
 
         for(let i = 0; i < position.length; i++){
-            squarePosition[position[i]] = {value: i+1,event: () =>{}, id: Math.random()}
+            squarePosition[position[i]] = {
+                value: i+1,
+                event: () =>{},
+                id: Math.random(),
+                test: {
+                    backgroundColor: '#ffffff',
+                    borderWidth: 1,
+                    borderRadius: 5,
+                    minWidth: 55,
+                    minHeight: 55,
+                    alignItems: "center",
+                    margin: 5,
+                }
+            }
         }
     }
 
+    const startHandler = () => {
+        boardHandler();
+        setGameBoard(squarePosition);
+        setStartVisible({display: "none"});
+        setGameVisible({display: "flex"});
+    }
 
 
 
     return (
         <SafeAreaView style={styles.container}>
-            <TouchableOpacity onPress={()=>{boardHandler(); setGameBoard(squarePosition)}}><Text>Test</Text></TouchableOpacity>
-            <FlatList
-                keyExtractor={()=> Math.random().toString()}
-                data={gameBoard}
-                numColumns={6}
-                renderItem={ ({item}) =>
-                    <View style={styles.board}>
-                        <TouchableOpacity style={styles.tile} onPress={item.event}>
-                            <Text style={{fontSize: 40}}>{item.value}</Text>
-                        </TouchableOpacity>
-                    </View>
-                }
-            />
+            <View style={startVisible}>
+                <View style={styles.startGame}>
+                    <TouchableOpacity style={styles.startButton}
+                                      onPress={startHandler}>
+                        <Text style={ {fontSize: 30} }>Start</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+            <View style={gameVisible}>
+                <FlatList
+                    style={{marginVertical: 100}}
+                    keyExtractor={()=> Math.random().toString()}
+                    data={gameBoard}
+                    numColumns={6}
+                    renderItem={ ({item}) =>
+                        <View>
+                            <TouchableOpacity activeOpacity={0.8} style={item.test} onPress={item.event}>
+                                <Text style={{fontSize: 30}}>{item.value}</Text>
+                            </TouchableOpacity>
+                        </View>
+                    }
+                />
+            </View>
         </SafeAreaView>
     )
 }
@@ -58,18 +99,18 @@ const styles = StyleSheet.create({
         paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
         minHeight: '10%',
     },
-    board: {
-
+    startGame:{
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: "50%",
     },
-    tile: {
-        backgroundColor: '#ffffff',
+    startButton: {
+        backgroundColor: "#62d653",
+        paddingVertical: 5,
+        paddingHorizontal: 10,
         borderWidth: 1,
-        borderRadius: 5,
-        minWidth: 55,
-        minHeight: 55,
-        alignItems: "center",
-        margin: 5,
-    }
+        borderRadius: 15,
+    },
 })
 
 export default ChimpGame
