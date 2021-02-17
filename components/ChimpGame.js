2bb3e746cfaee8248df57cbdfca318e3d549085d
Igor Lasota <igorlasota07@gmail.com>
2021-02-17 15:01:16 +0100

    Style Change

--- components/ChimpGame.js
+++ components/ChimpGame.js
@@ -152,7 +152,7 @@ function ChimpGame () {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#3ac1e3',
+        backgroundColor: '#fcf6f5',
         paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
         minHeight: '10%',
     },
@@ -162,11 +162,21 @@ const styles = StyleSheet.create({
         marginTop: "50%",
     },
     startButton: {
-        backgroundColor: "#62d653",
+        alignItems: "center",
+        marginTop: "15%",
+        marginBottom: "5%",
+        paddingHorizontal: 15,
         paddingVertical: 5,
-        paddingHorizontal: 10,
-        borderWidth: 1,
+        backgroundColor: "#3ac1e3",
         borderRadius: 15,
+        shadowColor: "#000000",
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.3,
+        shadowRadius: 5,
+        elevation: 7,
     },
     resultScreen: {
         alignItems: 'center',
@@ -175,12 +185,20 @@ const styles = StyleSheet.create({
     },
     endButton: {
         alignItems: "center",
-        marginTop: 15,
-        borderWidth: 1,
+        marginTop: "15%",
+        marginBottom: "5%",
         paddingHorizontal: 15,
         paddingVertical: 5,
-        backgroundColor: "#ddd457",
+        backgroundColor: "#3ac1e3",
         borderRadius: 15,
+        shadowColor: "#000000",
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.3,
+        shadowRadius: 5,
+        elevation: 7,
     }
 })
 
