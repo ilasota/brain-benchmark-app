e78fcca44bcfb318576a1b5568d35cdd10eb2435
Igor Lasota <igorlasota07@gmail.com>
2021-02-22 12:48:55 +0100

    Added colors.js

---
+++ assets/colors.js
@@ -0,0 +1,13 @@
+export const lightColors = {
+    background: '#fcf6f5',
+    button: '#3ac1e3',
+    text: '#222222',
+    buttonText: '#fff'
+};
+
+export const darkColors = {
+    background: '#fcf6f5',
+    button: '#3ac1e3',
+    text: '#222222',
+    buttonText: '#fff'
+};
