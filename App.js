9535714cd73e011481aff34edb786896e51cb371
Igor Lasota <igorlasota07@gmail.com>
2021-03-03 14:50:37 +0100

    Added Provider

--- App.js
+++ App.js
@@ -1,7 +1,10 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import { Provider } from 'react-redux'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
+import store from './data/store'
 
 import Home from "./components/Home";
 import NumberGame from "./components/NumberGame";
@@ -14,30 +17,32 @@ const Stack = createStackNavigator();
 export default function App() {
 
   return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="HomeScreen" screenOptions={{headerShown: false}}>
-          <Stack.Screen
-              name="Home"
-              component={Home}
-          />
-          <Stack.Screen
-              name="NumberGame"
-              component={NumberGame}
-          />
-          <Stack.Screen
-              name="ReactionGame"
-              component={ReactionGame}
-          />
-          <Stack.Screen
-              name="SpeedGame"
-              component={SpeedGame}
-          />
-          <Stack.Screen
-              name="ChimpGame"
-              component={ChimpGame}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <Provider store={store}>
+          <NavigationContainer>
+              <Stack.Navigator initialRouteName="HomeScreen" screenOptions={{headerShown: false}}>
+                  <Stack.Screen
+                      name="Home"
+                      component={Home}
+                  />
+                  <Stack.Screen
+                      name="NumberGame"
+                      component={NumberGame}
+                  />
+                  <Stack.Screen
+                      name="ReactionGame"
+                      component={ReactionGame}
+                  />
+                  <Stack.Screen
+                      name="SpeedGame"
+                      component={SpeedGame}
+                  />
+                  <Stack.Screen
+                      name="ChimpGame"
+                      component={ChimpGame}
+                  />
+              </Stack.Navigator>
+          </NavigationContainer>
+      </Provider>
   );
 }
 
