4d40a7dde2dfa6119365e08817427a142e8c7a32
Igor Lasota <igorlasota07@gmail.com>
2021-03-03 16:13:21 +0100

    Added reactionReducer.js

--- data/index.js
+++ data/index.js
@@ -1,8 +1,10 @@
 import numberReducer from "./numberReducer";
+import reactionReducer from "./reactionReducer";
 import {combineReducers} from "redux";
 
 const allReducers= combineReducers({
     numberReducer,
+    reactionReducer,
 })
 
 export default allReducers
---
+++ data/reactionReducer.js
@@ -0,0 +1,10 @@
+const reactionReducer = (state = [], action) => {
+    switch(action.type){
+        case 'SUBMIT_REACTION':
+            return [...state, action.payload];
+        default:
+            return state
+    }
+}
+
+export default reactionReducer
