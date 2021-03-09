9cc8c4f73ce5b1cf697150b15b11e939651865a0
Igor Lasota <igorlasota07@gmail.com>
2021-03-09 13:44:39 +0100

    Started Styling

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -7,9 +7,13 @@ function Login({ navigation }) {
     return(
         <SafeAreaView style={styles.container}>
             <View>
-                <Text>Text</Text>
-                <TouchableOpacity onPress={() => {navigation.navigate("Home")}}>
-                    <Text>home screen</Text>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={{fontSize: 20}}>Login</Text>
+                </TouchableOpacity>
+            </View>
+            <View>
+                <TouchableOpacity style={styles.button} onPress={() => {navigation.navigate("Home")}}>
+                    <Text style={{fontSize: 20}}>Play as guest</Text>
                 </TouchableOpacity>
             </View>
         </SafeAreaView>
@@ -22,6 +26,22 @@ const styles = StyleSheet.create({
         backgroundColor: '#fcf6f5',
         paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
         alignItems: "center",
+        justifyContent: "space-around"
+    },
+    button: {
+        alignItems: "center",
+        paddingHorizontal: 15,
+        paddingVertical: 5,
+        backgroundColor: "#3ac1e3",
+        borderRadius: 15,
+        shadowColor: "#000000",
+        shadowOffset: {
+            width: 0,
+            height: 3,
+        },
+        shadowOpacity: 0.3,
+        shadowRadius: 5,
+        elevation: 7,
     }
 });
 
