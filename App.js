9a12c4f0c7f74f0f9f7ed6de5fa17f42f77524ac
Igor Lasota <igorlasota07@gmail.com>
2021-06-02 14:26:26 +0200

    Set Login Screen as startScreen

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import "react-native-gesture-handler";
-import React from "react";
+import React, { useState } from "react";
 import { Provider } from "react-redux";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -17,11 +17,12 @@ import ChimpGame from "./components/ChimpGame";
 const Stack = createStackNavigator();
 
 export default function App() {
+  let startScreen = "Login";
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <NavigationContainer>
-          <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+          <Stack.Navigator initialRouteName={startScreen} screenOptions={{ headerShown: false }}>
             <Stack.Screen name="Login" component={Login} />
             <Stack.Screen name="Home" component={Home} />
             <Stack.Screen name="NumberGame" component={NumberGame} />
