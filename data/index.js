0572f28c12fa8416da083c20fa4e027fc5d09845
Igor Lasota <igorlasota07@gmail.com>
2021-06-09 13:32:25 +0200

    Added logInStatusReducer to Store

--- data/index.js
+++ data/index.js
@@ -2,6 +2,7 @@ import numberReducer from "./numberReducer";
 import reactionReducer from "./reactionReducer";
 import speedReducer from "./speedReducer";
 import chimpReducer from "./chimpReducer";
+import logInStatusReducer from "./logInStatusReducer";
 import { combineReducers } from "redux";
 
 const allReducers = combineReducers({
@@ -9,6 +10,7 @@ const allReducers = combineReducers({
   reactionReducer,
   speedReducer,
   chimpReducer,
+  logInStatusReducer,
 });
 
 export default allReducers;
