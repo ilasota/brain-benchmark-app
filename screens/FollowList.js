8d20f73ea2a43308d54b620759364ccf6b10b959
Igor Lasota <igorlasota07@gmail.com>
2021-08-14 12:41:50 +0200

    FollowLsit Testing

--- screens/FollowList.js
+++ screens/FollowList.js
@@ -1,13 +1,75 @@
 import "react-native-gesture-handler";
-import React from "react";
-import { StyleSheet, View, Text, SafeAreaView, StatusBar, TouchableOpacity, Image } from "react-native";
+import React, { useState } from "react";
+import NetInfo from "@react-native-community/netinfo";
+import {
+  StyleSheet,
+  View,
+  Text,
+  SafeAreaView,
+  StatusBar,
+  TouchableOpacity,
+  Image,
+  FlatList,
+  TextInput,
+} from "react-native";
+import { useSelector, useDispatch } from "react-redux";
+
+import { followSubmit } from "../data/actions";
 
 function FollowList({ navigation }) {
+  const [noConnectionVisible, setNoConnectionVisible] = useState();
+  const [listVisible, setListVisible] = useState();
+  const [followInput, setFollowInput] = useState("");
+  const followListData = useSelector((state) => state.followListReducer);
+  const [listData, setListData] = useState(followListData);
+
+  const dispatch = useDispatch();
+
+  NetInfo.fetch().then((state) => {
+    if (!state.isConnected) {
+      setNoConnectionVisible(styles.visible);
+      setListVisible(styles.invisible);
+    } else {
+      setNoConnectionVisible(styles.invisible);
+      setListVisible(styles.visible);
+    }
+  });
+
+  const inputHandler = () => {
+    setListData([...listData, followInput]);
+    setFollowInput("");
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <TouchableOpacity style={styles.arrowPlacement} onPress={() => navigation.navigate("Home")}>
         <Image source={require("../assets/backarrow.png")} style={styles.backarrow} />
       </TouchableOpacity>
+      <View style={noConnectionVisible}>
+        <View style={styles.netInfoPlacement}>
+          <Text style={styles.mediumFontBold}>No Internet Connection</Text>
+        </View>
+      </View>
+      <View style={listVisible}>
+        <TextInput style={styles.input} onChangeText={(input) => setFollowInput(input)} value={followInput} />
+        <TouchableOpacity onPress={inputHandler}>
+          <Text>daiughdwi</Text>
+        </TouchableOpacity>
+        <FlatList
+          data={data}
+          onSubmitEnding={inputHandler}
+          keyExtractor={() => Math.random().toString()}
+          renderItem={({ item }) => (
+            <View>
+              <TouchableOpacity>
+                <View>
+                  <Text>{item}</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+          )}
+        />
+      </View>
     </SafeAreaView>
   );
 }
@@ -25,6 +87,27 @@ const styles = StyleSheet.create({
   backarrow: {
     width: 30,
     height: 25,
+  },
+  netInfoPlacement: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  visible: {
+    display: "flex",
+  },
+  invisible: {
+    display: "none",
+  },
+  mediumFontBold: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#484848",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
 });
 
