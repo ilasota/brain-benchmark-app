bcff1394ae9f8cdb6b10d4121dce159cb586bfa6
Igor Lasota <igorlasota07@gmail.com>
2021-08-18 15:38:49 +0200

    Added getting followed scores, first design tests

--- screens/FollowedProfile.js
+++ screens/FollowedProfile.js
@@ -1,19 +1,80 @@
 import "react-native-gesture-handler";
-import React from "react";
-import { useSelector } from "react-redux";
-import { StyleSheet, View, Text, SafeAreaView, StatusBar, TouchableOpacity, Image } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  SafeAreaView,
+  StatusBar,
+  TouchableOpacity,
+  Image,
+  ActivityIndicator,
+  ScrollView,
+} from "react-native";
+import { API_LINK } from "@env";
 
 function FollowedProfile({ navigation, route }) {
+  const [numberScore, setNumberScore] = useState([]);
+  const [speedScore, setSpeedScore] = useState([]);
+  const [reactionScore, setReactionScore] = useState([]);
+  const [chimpScore, setChimpScore] = useState([]);
+  const [loadingVisible, setLoadingVisible] = useState(styles.visible);
+  const [scoresVisible, setScoresVisible] = useState(styles.invisible);
+
   const { followedName } = route.params;
+
+  useEffect(() => {
+    const ac = new AbortController();
+    fetch(`${API_LINK}/${followedName}/scores`)
+      .then((res) => res.json())
+      .then((json) => {
+        setNumberScore(json.numberScore);
+        setSpeedScore(json.speedScore);
+        setReactionScore(json.reactionScore);
+        setChimpScore(json.chimpScore);
+      })
+      .catch((err) => console.error(err))
+      .finally(() => {
+        setLoadingVisible(styles.invisible);
+        setScoresVisible(styles.visible);
+      });
+    return () => {
+      setNumberScore([]);
+      setSpeedScore([]);
+      setReactionScore([]);
+      setChimpScore([]);
+    };
+  }, []);
 
   return (
     <SafeAreaView style={styles.container}>
-      <TouchableOpacity style={styles.arrowPlacement} onPress={() => navigation.navigate("Home")}>
+      <TouchableOpacity style={styles.arrowPlacement} onPress={() => navigation.navigate("FollowList")}>
         <Image source={require("../assets/backarrow.png")} style={styles.backarrow} />
       </TouchableOpacity>
       <View style={styles.header}>
         <Text style={styles.mediumFontBold}>{followedName}</Text>
       </View>
+      <ActivityIndicator size="small" color="#000" style={loadingVisible} />
+      <ScrollView contentContainerStyle={scoresVisible}>
+        <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-evenly" }}>
+          <Image style={styles.image} source={require("../assets/numbers.png")} />
+          <View style={{ flex: 1 }}>
+            <View style={{ alignItems: "center" }}>
+              <Text>Number Game</Text>
+            </View>
+            <View style={{ flexDirection: "row", justifyContent: "space-evenly", flex: 1 }}>
+              <View>
+                <Text>High Score</Text>
+                <Text>12</Text>
+              </View>
+              <View>
+                <Text>Average Score</Text>
+                <Text>10,5</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -38,9 +99,19 @@ const styles = StyleSheet.create({
     paddingVertical: 3,
     paddingHorizontal: 10,
   },
+  image: {
+    height: 60,
+    width: 60,
+  },
   mediumFontBold: {
     fontSize: 30,
     fontWeight: "bold",
+  },
+  visible: {
+    display: "flex",
+  },
+  invisible: {
+    display: "none",
   },
 });
 
